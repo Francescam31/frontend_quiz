4c76176f2b5ea9eee479c7a16c6054a802723991
Faran71 <muhammadfaran01.mfs@gmail.com>
2023-09-11 14:40:02 +0100

    quizcontainer made

--- src/containers/QuizContainer.js
+++ src/containers/QuizContainer.js
@@ -1,6 +1,21 @@
-
+import { useEffect, useState } from "react";
 
 const QuizContainer = () => {
+
+    const [questions, setQuestions] = useState([]);
+    const [previousQuestions, setPreviousQuestions] = useState([]);
+    const [timer, setTimer] = useState(30);
+    const [aggregateScore, setAggregateScore] = useState(0);
+
+    const fetchQuestions = async () => {
+        const response = await fetch("http://localhost:8080/questions") ;
+        const questionsData = await response.json();
+        setQuestions(questionsData);
+    }
+
+    useEffect(() => {
+        fetchData();
+    },[]);
 
     return (
         <div>
