b763c0c6f553cd19789956a23380c09756502510
essamcreates <mrm.hussain@hotmail.co.uk>
2023-09-14 15:25:01 +0100

    CSS styling work + background img import

--- src/App.js
+++ src/App.js
@@ -5,12 +5,24 @@ import { useState } from 'react';
 function App() {
 
   return (
-    <>
+    <div
+    style={{
+      backgroundImage: `url('./background/worldmap.png')`, 
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      width: '100vw',
+      height: '100vh',
+      position: 'relative'
+      }}
+      >
+    <div className='QuizContainer'>    
     <QuizContainer />
-    </>
+    </div> 
+    </div>
   );
 
  
 }
 
 export default App;
+
