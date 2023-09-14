c1b91180340825ca51b3a60db91695ff3bef8bc6
amelieboydshire <aboydshire10@gmail.com>
2023-09-14 11:54:03 +0100

    attempt on making answer button click red or green

--- src/components/quizPage/QuizList.js
+++ src/components/quizPage/QuizList.js
@@ -4,13 +4,17 @@ import '../../App.css';
 
 const QuizList = ({question, setAggregateScore, aggregateScore, setButtonHidden, buttonPressed, setButtonPressed, answerButtonClicked, setAnswerButtonClicked, setTimer, timer}) => {
 
+    const [isAnswerCorrect, setIsAnswerCorrect] = useState(null);
     
+    // const [active, setActive] = useState(false);
 
     const returnQuestion = () => {
         return <h1 className='question'>{question.questionText}</h1>
     }
 
-    const handleAnswerButtonClick = (answer) => {
+
+
+    const handleAnswerButtonClick = (answer, e) => {
         if (answerButtonClicked === false && timer>0) {
             if (answer === question.correctAnswer) {
                 console.log('correctAnswer');
@@ -19,7 +23,9 @@ const QuizList = ({question, setAggregateScore, aggregateScore, setButtonHidden,
                 console.log(aggregateScore);
                 setAnswerButtonClicked(true);
                 setTimer(0);
-                setButtonPressed(true)
+                setButtonPressed(true);
+                // setActive(answer===question.correctAnswer);
+                setIsAnswerCorrect(true)
             } else{
                 console.log('incorrectAnswer');
                 setAggregateScore([...aggregateScore,0])
@@ -27,15 +33,28 @@ const QuizList = ({question, setAggregateScore, aggregateScore, setButtonHidden,
                 console.log(aggregateScore);
                 setAnswerButtonClicked(true);
                 setTimer(0);
-                setButtonPressed(true)
+                setButtonPressed(true);
+                setIsAnswerCorrect(false)
             } 
         }   
         // figure out how to enter a 0 value to null
     }
 
+    // function selectButtonColour(e) {
+    // const answerButtonClicked = e.target;
+    // const isCorrect = answerButtonClicked.question.correctAnswer === "true";
+    // if(isCorrect){
+    //     answerButtonClicked.classList.add("correct");
+    // } else {
+    //     answerButtonClicked.classList.add("incorrect");
+    // }}
+
+
     const returnAnswers = question.options.map((answer, index) => {
         return  <button key={index} onClick={() => handleAnswerButtonClick(answer)} disabled={buttonPressed} >{answer}</button>
     }) 
+
+
     
 
     return (
