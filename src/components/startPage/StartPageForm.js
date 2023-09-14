2292a1a35b3d204afdc04d1f2e015ee9b0f982b8
Faran71 <muhammadfaran01.mfs@gmail.com>
2023-09-14 12:22:50 +0100

    trying to add motion

--- src/components/startPage/StartPageForm.js
+++ src/components/startPage/StartPageForm.js
@@ -1,19 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link , useNavigate} from "react-router-dom";
 import "./StartPageForm.css";
 import { motion } from "framer-motion";
 
 const StartPageForm = ({username, setUsername, postUser}) => {
+    const navigate = useNavigate();
 
     const handleFormSubmit = (event) => {  
         event.preventDefault()
         postUser("")
+        navigate("/QuizPage")
     }
 
     return (
         <motion.div
         className="start-page-container"
         initial={{ opacity: 0 }}
-        animate={{ opacity: 1, transtion : {duration: 50} }}
+        animate={{ opacity: 1, transtion : {duration: 3} }}
         >
             <h1>FrontEnd of the world 🌍💡</h1>
             <h2>Your knowledge Our arena</h2>
